5e4f3974c16d4d1eae29fc0d728c538e6c9d323b
gapanovichV <vladgapanovich172@gmail.com>
2023-02-11 23:39:34 +0300

    add package classnames

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import cn from 'classnames';
 
 function Modal({ active, setActive, children }) {
 	return (
-		<div className={active ? 'modal active' : 'modal'} onClick={() => setActive(false)}>
+		<div className={cn('modal', { active: active })} onClick={() => setActive(false)}>
 			<div className="modal-content" onClick={(e) => e.stopPropagation()}>
 				{children}
 			</div>
